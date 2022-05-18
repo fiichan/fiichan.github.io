6359582d8a31941bf9dd1ff868a3d037bb3f4d2e
Sofie C <sofichaves109@gmail.com>
2022-05-18 04:20:14 +0200

    votes + server fix
    send votes
    start vote logic fx
    fix server fetches
    TODO fix nomenclature

--- expo/js/script.js
+++ expo/js/script.js
@@ -2,6 +2,7 @@ const moodColors = ["#F45453", "#FC9B21", "#FDCF3D", "#91E777", "#00C96C"];
 const moodLabels = ["hated", "disliked", "didn't mind", "liked", "loved"];
 
 const moodId = "mood-0";
+const percentId = "percent-0";
 const filePath = "img/moods/";
 const fileNames = ["01-Saddest", "02-Sad", "03-Meh", "04-Happy", "05-Happiest"];
 const fileActive = "-Active.png";
@@ -12,10 +13,20 @@ const videoPath = "videos/";
 
 /* App Key https://keyvalue.immanuel.co/ */
 const appkey = "4arncai3";
-var mood = 100;
+var mood = 3;
 var visitors = 2;
 
 var activeBtn = "";
+var selectedVote = 0;
+
+
+/* Votes */
+var vote01 = 0;
+var vote02 = 1;
+var vote03 = 2;
+var vote04 = 3;
+var vote05 = 4;
+var votes = [0,0,0,0,0];
 
 /*MAIN*/
 document.addEventListener("DOMContentLoaded", function(event) { 
@@ -24,15 +35,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     /*visitors = await myGet("visitors");
     alert(visitors);*/
     setInterval(refreshAll, 5000);
-
 });
 
 function refreshAll() {
     getVisitors();
+    for(var i=0; i<5; ++i) {
+        getVote(i);
+    }
+    console.log(votes);
     document.getElementById('visitor-count').innerHTML = visitors;
 
     getMood();
     moodSwitch();
+    win();
 }
 /*function refreshAll() {
     Promise.all([getVisitors, getMood]).then(values => {
@@ -47,10 +62,18 @@ document.body.addEventListener("click", () => {
     vid.play();
 })
 
+/*Voting - Submit*/
+function vote() {
+    console.log("ActiveBtn " + selectedVote);
+    myIncrement("visitors");
+    myIncrement("vote0" + (selectedVote + 1));
+    getVote(selectedVote);
+}
+
 /*Voting - Mood Buttons*/
 function moodBtn(clickedId, index) {
     var btn = document.getElementById(clickedId);
-
+    selectedVote = index;
     if (btn.classList.contains('active')) {
         btn.classList.remove("active");
         $( "#" + clickedId + " div img").attr("src", filePath + fileNames[index] + fileActive);
@@ -75,39 +98,52 @@ function moodBtn(clickedId, index) {
     }
 }
 
+/* Define % and mood */
+function win() {
+    var max = Math.max(...votes);
+    mood = votes.indexOf(max) + 1;
+    console.log({
+        votes,
+        mood
+    });
+}
+
 /* Main Screen - Mood Functions*/
 function moodSwitch() {
-    if (mood <= 20) {
+    if (mood == 1) {
         moodChange(0);
     }
-    if (mood > 20 && mood <= 40) {
+    if (mood == 2) {
         moodChange(1);
     }
-    if(mood > 40 && mood <= 60) {
+    if(mood == 3) {
         moodChange(2);
     }
-    if (mood > 60 && mood <= 80) {
+    if (mood == 4) {
         moodChange(3);
     }
-    if (mood >= 80 && mood <= 100) {
+    if (mood == 5) {
         moodChange(4);
     }
 
 }
 
 function moodChange(index) {
+    moodReset();
+
     document.getElementById("mood-label").style.color = moodColors[index];
     document.getElementById('mood-label').innerHTML = moodLabels[index];
 
-    moodReset();
+    document.getElementById(percentId + (index+1)).style.color = moodColors[index];
     document.getElementById(moodId + (index+1)).src= filePath + fileNames[index] + fileActive;
-    document.getElementById("mood-cover").style.width = 100-mood + '%';
+    /*document.getElementById("mood-cover").style.width = 100-mood + '%';*/
     
     vid.src = videoPath + fileNames[index] + ".mp4";
 }
 function moodReset() {
     for(i=0;i<5;++i) {
         document.getElementById(moodId + (i+1)).src= filePath + fileNames[i] + ".png";
+        document.getElementById(percentId + (i+1)).style.color = "#E3E3E3";
     }
 }
 
@@ -130,13 +166,20 @@ async function myIncrement(itemkey) {
 
 function getVisitors() {
     return myGet("visitors").then(function (result) {
-      visitors = result;
+      visitors = parseInt(result);
     });
 }
 
 function getMood() {
     return myGet("mood").then(function (result) {
-      mood = result;
+      mood = parseInt(result);
+    });
+}
+
+
+function getVote(index) {
+    return myGet("vote0" + (index + 1)).then(function (result) {
+        votes[index] = parseInt(result);
     });
 }
 
